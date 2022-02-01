e7e61846f8f729f86587460047a645da06260a10
MaksaDev <jokic.maksim17@gmail.com>
2022-02-01 17:58:25 +0100

    basic view + bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container-lg" style="margin: 0 auto;">
+
+    <div class="raw mt-5">
+
+        <div class="col-lg-44 col-md4 col-sm-12 text-center mb-3 mx-5">
+
+            <div class="card" style="width: 28rem;">
+
+            <img/>
+            <div class="card-body">
+                <div class="card-title">title</div>
+                <div class="card-text">content</div>
+            </div>
+        
+        
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+</div>
+
+
+
+
+
+@endsection
